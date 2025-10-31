4df7e75d3ea98d1b10b46ca70723689402af5480
isa-yan <isabel.yyj.yan@gmail.com>
2025-10-31 01:07:09 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+
+var agree = alert("By entering this website you agree that you understand that we (me and my friend) are not professional policy analyzers in any way, shape, or form, just teenagers that want to help out their local community. We apologize in advance for any bias or incorrect information that we may have presented. This website was not made to discredit, insult, or defame any candidate, party member, or political figure. We encourage readers to verify statements from official campaign statements. This site is independently made and is not affiliated with any candidate, campaign, or organization. And that we are not liable for any misunderstandings or anything else regarding this wesbite for the election in a court of law")
+
+console.log(agree)
+
+
